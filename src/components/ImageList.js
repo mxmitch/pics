43fb57a511df5c26be9ce05a8e0cae96a017a24f
Mitch Lum <mklum88@gmail.com>
2020-05-20 12:38:09 -0700

    added css to display images in grid

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,12 @@
+import './ImageList.css';
 import React from 'react';
 
 const ImageList = (props) => {
-  const images = props.images.map((image) => {
-    return <img src={image.urls.small} />;
+  const images = props.images.map(({ description, id, urls }) => {
+    return <img key={id} alt={description} src={urls.small} />;
   });
 
-  return <div>{images}</div>;
+  return <div className='image-list'>{images}</div>;
 };
 
 export default ImageList;
