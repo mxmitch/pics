e6e0e173c895d7a48c16aecc2675a62ffe5c69aa
Mitch Lum <mklum88@gmail.com>
2020-05-20 12:02:45 -0700

    changed to async call and added setState

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,23 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization: 'Client-ID WIIW6rkWCX7svRG-re5j41CuBLOyaMTBNZ15WPLTmOU'
       }
     });
-  }
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className='ui container' style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
